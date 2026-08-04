45d714d5a0265b2474f7c7c9a998bb264b2f8cc9
Румия Абдряхимова <rumiaabdrahimova@MacBook-Air-Rumia.local>
2026-08-05 00:00:20 +0400

    Add iPhone and PWA app icons

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,18 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
+            src: '/icons/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: '/icons/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+          {
             src: '/icons/app-icon-backpack-v1.svg',
             sizes: 'any',
             type: 'image/svg+xml',
